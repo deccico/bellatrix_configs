391186bb389c88ef71e5f30a851d8bf231f0a59a
Adri?n Deccico <deccico@gmail.com>
2012-02-12 02:18:52 +1100

    adding first cut of instructions to provision django_gunicorn_nginx in a Ubuntu machine

---
+++ bellatrix-configs/lib/cmds.py
@@ -0,0 +1,81 @@
+""" 
+Common utilities, meant to be used as build blocks.
+Please, keep every block independent from the others. 
+
+Every set of command needs to be stored into a list of strings.
+If any of the commands fails, the execution will be interrupted. It's convenient that you add a small test at the very end of each set. 
+
+For example:
+
+install_pip = [
+               "touch new_file",
+               "cat new_file",
+               ] 
+               
+It's a example of a single-purposed operation that performs a test at the end. 
+Should something happened to the operation, 'cat new_file' will fail. 
+Additionally you will be able to verify the content of the file.               
+"""
+
+CONSTANTS
+#http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=422427
+apt_prefix = "export PATH=$PATH:/sbin:/usr/sbin:/usr/local/sbin;export TERM=linux; "
+apt_get_and_options = "sudo /usr/bin/apt-get -q -y -o DPkg::Options::=--force-confold --fix-missing " 
+apt_install = apt_get_and_options + "install "
+apt_get_install = apt_prefix + apt_install 
+
+
+def apt_get_install(package):
+    return [apt_get_install + "package"]
+
+def pip_install(package, prefix="sudo", verify=True, verification_command=None):
+    cmds = [prefix + " pip install " + package + " --upgrade"]
+    if command_verification:
+        cmds += package + " --version" if verification_command == None else command_verification
+    return cmds
+
+def createVirtualEnv(env_name):
+    cmds = ["virtualenv --no-site-packages " + env_name]
+
+def executeInVirtualEnv(env, cmd):
+    return ["source " + env + os.path.sep + "bin" + os.path.sep + "activate && " + cmd] 
+
+def installPackageInVirtualEnv(env, package, verify=True, verification_command=None, prefix=""):
+    cmds = pip_install(package, prefix, verify, verification_command) 
+    cmd = ""
+    ampersand = " && "
+    #we need to get a single command so the work in the virtual environment
+    for c in cmds:
+        cmd += c + ampersand 
+    cmd = cmd[:len(cmd) - len(ampersand)]   #cut last &&
+    return executeInVirtualEnv(env, cmd) 
+
+install_pip = [
+               apt_get_install("python-pip"),
+               pip_install("pip")    #why? to upgrade pip package using pip itself
+               ]
+
+apt_get_update = [
+                  apt_get_and_options + "update"
+                  ]
+
+#from: http://wiki.nginx.org/Install#Ubuntu_PPA
+install_nginx = ["sudo add-apt-repository ppa:nginx/stable -y"] \
+                + apt_get_update \
+                + apt_get_install("nginx")
+                
+def create_django_project(project_name, dir="." + os.path.sep):                
+    return ["cd " + dir + " && django-admin.py startproject " + project_name]
+
+def wget(url, dest=None):
+    return ["wget --no-check-certificate " + url + "" if dest == None else " " + dest]
+
+def sudo(cmds):
+    for i in range(cmds):
+        cmds[i] = "sudo " + cmds[i]
+    return cmds
+
+def chmod(mode, file, options=""):
+    return ["chmod " + options + " " + mode + " " + file]    
+    
+
---
+++ bellatrix-configs/ubuntu_django_nginx_gunicorn.py
@@ -0,0 +1,69 @@
+"""This configuration holds the list of cmds that are going to be applied to a list of ami's. 
+This is part of the normal Bellatrix process. The new configuration will be burned into a new ami."""
+
+#list of ami's to process with the below cmds
+amis = [
+       ["ami-fd589594",  "ubuntu1104-ff36-mysql51-x64"],
+       ]
+
+#common variables
+burn_ami_at_the_end=True    #decide whether or not burning the images at the end
+skip_me = False             #decide whether to skip or not this configuration
+user = "ubuntu"             #user of the ami's 
+
+#this values will be applied to the instance that is going to be configured with bellatrix bewitch_ami
+#once you have your ami configured, then you can launch it with a different configuration 
+key_name = "elasticbamboo"          #Name of the key-pair name that will be applied to your instance. 
+security_groups = "elasticbamboo"   #comma separated list (with no spaces) of the security groups that will be applied to the new instance. It can be only one. Usually is. e.g. mysecurity_group
+instance_type = "t1.micro"          #type of instance that will be used for applying the configuration. Usually t1.micro should be enough. List of codes here: http://aws.amazon.com/ec2/instance-types for more
+
+
+#------------------------------------------------------------------------------------------------
+from bellatrix-configs.lib import cmds
+
+env = "django_app"
+project_name = "app"
+django_app_dir = "/home/ubuntu/django_app"
+
+def configureNginx():
+    cmds = [
+            #prepare directories
+            "sudo mkdir -p /opt/django/logs/nginx/", 
+            #Create directories and softlinks for static content and templates
+            + "mkdir $HOME/django_app/static",
+            + "mkdir $HOME/django_app/templates",
+            + "sudo ln -s $HOME/django_app/static /opt/django",
+            #download and set up Nginx configuration. Basically it will listen in port 80 and forward to port 8000 dynamic content
+            + "sudo mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default.backup"
+            ]
+    cmds += wget("https://bitbucket.org/deccico/django_gunicorn/raw/tip/server/etc/nginx/sites-available/default")
+    cmds.append("sudo cp default /etc/nginx/sites-available/default")
+    return cmds
+
+
+#list of cmds to execute
+commands = cmds.install_pip() 
+commands += cmds.pip_install("virtualenv") \
+commands += cmds.install_nginx \
+commands += cmds.createVirtualEnv(env_name) \
+commands += cmds.installPackageInVirtualEnv(env, package="django", verification_command="django-admin.py --version")
+commands += cmds.installPackageInVirtualEnv(env, package="gunicorn")
+commands += cmds.executeInVirtualEnv(env, create_django_project(project_name, dir=env + os.path.sep))
+commands += configureNginx()
+
+#setting up Django app
+commands += cmds.wget("https://bitbucket.org/deccico/django_gunicorn/raw/tip/django_app/settings.py", "/home/ubuntu/django_app/app/settings.py")
+commands += cmds.wget("https://bitbucket.org/deccico/django_gunicorn/raw/tip/django_app/urls.py", 
+                      django_app_dir + "/app/urls.py")
+commands += cmds.wget("https://bitbucket.org/deccico/django_gunicorn/raw/1587f68db41e/templates/test_static.html",
+                      django_app_dir + "/templates/test_static.html")
+commands += cmds.wget("https://bitbucket.org/deccico/django_gunicorn/raw/tip/static/django.png",
+                      django_app_dir + "/static")
+
+
+#setting up Upstart to automatically launch Django application 
+commands += cmds.wget("https://bitbucket.org/deccico/django_gunicorn/raw/tip/run.sh",
+                      "/home/ubuntu/django_app/run.sh")
+commands += cmds.chmod("a+x", django_app_dir + "/run.sh"):
+commands += cmds.sudo(cmds.wget("https://bitbucket.org/deccico/django_gunicorn/raw/tip/server/etc/init/django_app.conf",
+                      "/etc/init/django_app.conf"))
