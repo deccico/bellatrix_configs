6bd3743403501917045cf7592a7e45f40ff25a5e
Adri?n Deccico <deccico@gmail.com>
2012-02-18 20:27:48 +1100

    enhacing description

--- push.sh
+++ push.sh
@@ -1,2 +1,2 @@
-echo commiting with message $1
-hg commit -m "$1" && hg push && wget http://localhost:8080/job/02-provision%20instance/build?token=secret0
+echo commiting with message $1 then pushing and then executing Jenkins job
+hg commit -m "$1" && hg push && wget -q http://localhost:8080/job/02-provision%20instance/build?token=secret0
