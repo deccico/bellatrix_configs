0a9684a26c67013bd0a48f558e4f1145b4aac362
Adri?n Deccico <deccico@gmail.com>
2012-03-19 01:51:43 +1100

    adding simple provision example

---
+++ bellatrix_configs/simple_provision_test.py
@@ -0,0 +1,13 @@
+"""
+Simple example of a configuration file for the provisioning command.
+"""
+
+
+#commands library from Bellatrix
+#The source file can be found here:     https://bitbucket.org/adeccico/bellatrix/src/tip/bellatrix/lib/cmds.py
+# and the documentation here: http://bellatrix.readthedocs.org/en/latest/source/ref/bellatrix.lib.html#module-bellatrix.lib.cmds
+from bellatrix.lib import cmds
+commands = cmds.apt_get_update()
+commands = cmds.install_pip()
+commands += cmds.pip_install("virtualenv")
+commands += ['echo "Adding my own command :)" > test', 'cat test']
