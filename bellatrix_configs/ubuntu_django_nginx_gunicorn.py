6e8d9dc6b3a69fd059c6eb45b18dda1fd1585ff9
Adri?n Deccico <deccico@gmail.com>
2012-04-01 18:10:00 +1000

    moving ami's to bewitch section

--- bellatrix_configs/ubuntu_django_nginx_gunicorn.py
+++ bellatrix_configs/ubuntu_django_nginx_gunicorn.py
@@ -8,21 +8,18 @@ The available set of commands can be found in https://bitbucket.org/adeccico/bel
 import logging
 import os
 
-#list of ami's to process with the below cmds
-amis = [
-       ["ami-fd589594",  "ubuntu1104-ff36-mysql51-x64"],
-       ]
-
 #common variables
 user = "ubuntu"             #user of the ami's 
 
-#this values will be applied to the instance that is going to be configured with bellatrix bewitch_ami
-#once you have your ami configured, then you can launch it with a different configuration 
+#bellatrix bewitch specific configurations:
+#bellatrix bewitch is a macro command will start an instance, provision it and finally burn it. 
+#You can achieve the same results using the start, provision and then burn commands.   
+amis = [["ami-fd589594",  "ubuntu1104-ff36-mysql51-x64"],] #list of AMI's that 'bellatrix bewitch' will configure.
 key_name = "key"          #Name of the key-pair name that will be applied to your instance. 
 security_groups = "default"   #comma separated list (with no spaces) of the security groups that will be applied to the new instance. It can be only one. Usually is. e.g. mysecurity_group
-instance_type = "t1.micro"          #type of instance that will be used for applying the configuration. Usually t1.micro should be enough. List of codes here: http://aws.amazon.com/ec2/instance-types for more
-
+instance_type = "t1.micro"    #type of instance that will be used for applying the configuration. Usually t1.micro should be enough. List of codes here: http://aws.amazon.com/ec2/instance-types for more
 #------------------------------------------------------------------------------------------------
+
 from bellatrix.lib import cmds
 
 env = "django_app"
