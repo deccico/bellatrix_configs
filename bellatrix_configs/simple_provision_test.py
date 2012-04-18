d2bf49a2639b46a6f57e03236bfa416e9eab0b11
Adri?n Deccico <deccico@gmail.com>
2012-04-18 22:40:54 +1000

    fixing simple_provision_test commands

--- bellatrix_configs/simple_provision_test.py
+++ bellatrix_configs/simple_provision_test.py
@@ -8,6 +8,6 @@ Simple example of a configuration file for the provisioning command.
 # and the documentation here: http://bellatrix.readthedocs.org/en/latest/source/ref/bellatrix.lib.html#module-bellatrix.lib.cmds
 from bellatrix.lib import cmds
 commands = cmds.apt_get_update()
-commands = cmds.install_pip()
+commands += cmds.install_pip()
 commands += cmds.pip_install("virtualenv")
 commands += ['echo "Adding my own command :)" > test', 'cat test']
